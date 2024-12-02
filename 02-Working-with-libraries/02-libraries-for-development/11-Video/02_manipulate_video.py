c8bbfe3fd6d6c939065a620ad7199af778611a0b
ztarbug <kronprinzfriedrich@gmx.de>
2024-12-02 21:37:22 +0100

    more video examples

--- 02-Working-with-libraries/02-libraries-for-development/11-Video/02_manipulate_video.py
+++ 02-Working-with-libraries/02-libraries-for-development/11-Video/02_manipulate_video.py
@@ -1,24 +1,22 @@
 import cv2
+import numpy as np
 
 def display_frames_webcam():
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(2)
 
     cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
     cv2.namedWindow('webcam', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('webcam', 1280, 720)
 
     while True:
         ret, frame = cam.read()
-        frame = manipulate_video("draw", frame)
+        frame = manipulate_video("corner", frame)
         cv2.imshow('webcam', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cam.release()
             cv2.destroyAllWindows()
-            out.release()
             break
 
 def manipulate_video(mode, frame):
@@ -37,10 +35,21 @@ def manipulate_video(mode, frame):
             frame = cv2.blur(frame, (5, 5))
         case "edges":
             frame = find_edges(frame)
+        case "corner":
+            frame = find_corners(frame)
     return frame
 
 def find_edges(frame):
     # TODO
+    return frame
+
+def find_corners(frame):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    dst = cv2.cornerHarris(frame, 3, 5, 0.1)
+    corners = dst > 0.05 * dst.max()
+    coord = np.argwhere(corners)
+    for y, x in coord:
+        cv2.circle(frame, (x,y), 3, (0,0,255), -1)    
     return frame
 
 if __name__ == "__main__":
